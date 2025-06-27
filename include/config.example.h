ad42dd5a2a810c32c8706cd51b08bcff2cd4f640
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-06-27 16:22:22 -0400

    Connect to WiFi

---
+++ include/config.example.h
@@ -0,0 +1,10 @@
+// Rename to config.h
+// Temporary, will later use WiFi provisinioning
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* ssid = "your-ssid";         // Replace with your WiFi SSID
+const char* password = "your-password"; // Replace with your WiFi password
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
+#include "config.h"
 #include <Arduino.h>
+#include <WiFi.h>
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.print("\nConnected to WiFi!\nIP Address:");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-}
+void loop() {}
