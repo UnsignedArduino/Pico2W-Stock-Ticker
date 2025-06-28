d1654257247883d00b7fc90e186a2117d8cf137b
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-06-28 15:04:08 -0400

    Decode the protobuf messages

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "config.h"
 #include <Arduino.h>
 #include <ArduinoHttpClient.h>
+#include <ArduinoJson.h>
+#include <StreamUtils.h>
 #include <WiFi.h>
+#include <base64.hpp>
+#include <pb_decode.h>
+#include <pricing.pb.h>
 
 WiFiClientSecure wifiClient;
 WebSocketClient wsClient = WebSocketClient(wifiClient, host, port);
@@ -33,17 +38,49 @@ void loop() {
   }
   Serial1.println("Connected to websocket");
 
-  Serial1.println("Subscribing to BTC-USD ticker...");
+  Serial1.println("Subscribing to tickers...");
   wsClient.beginMessage(TYPE_TEXT);
   wsClient.print("{\"subscribe\": [\"BTC-USD\"]}");
   wsClient.endMessage();
+  Serial1.println("Subscribed to tickers");
 
   while (wsClient.connected()) {
     if (wsClient.parseMessage() > 0) {
-      Serial1.print("Received message: ");
-      Serial1.println(wsClient.readString());
-    } else {
-      Serial1.println("No message received");
+      JsonDocument doc;
+
+      ReadBufferingClient bufferedClient(wsClient, 256);
+      DeserializationError error = deserializeJson(doc, bufferedClient);
+
+      if (error) {
+        Serial.print("deserializeJson() failed: ");
+        Serial.println(error.c_str());
+      } else {
+        const char* type = doc["type"];
+        if (strcmp(type, "pricing") == 0) {
+          const char* message = doc["message"];
+          Serial1.print("Received pricing message: (base64 of protobuf)\n  ");
+          Serial1.println(message);
+          // First decode from base64 into a byte array and length
+          // The messages are usually 160 characters long, so decoded will be
+          // 120 bytes long. We'll have 192 bytes to be safe.
+          const size_t decodedMaxSize = 192;
+          uint8_t decoded[decodedMaxSize];
+          const size_t decodedLength = decode_base64(
+            reinterpret_cast<const unsigned char*>(message), decoded);
+          // Then decode the protobuf message
+          pb_istream_t istream = pb_istream_from_buffer(decoded, decodedLength);
+          PricingData pricingData = PricingData_init_zero;
+          Serial1.println("Decoded Pricing Data:");
+          pb_decode(&istream, PricingData_fields, &pricingData);
+          Serial1.printf("  ID: %s\n", pricingData.id);
+          Serial1.printf("  Price: %.2f\n", pricingData.price);
+          Serial1.printf("  Change: %+.2f\n", pricingData.change);
+          Serial1.printf("  Change percent: %+.2f%%\n",
+                         pricingData.change_percent);
+        } else {
+          Serial1.println("Received non-pricing message, ignoring.");
+        }
+      }
     }
 
     delay(1000);
