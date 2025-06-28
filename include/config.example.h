b05b91f3e262a4022813db9915bb06581bfea411
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-06-27 23:37:53 -0400

    Able to connect to Yahoo Finance websocket API

--- include/config.example.h
+++ include/config.example.h
@@ -4,7 +4,12 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <Arduino.h>
+
 const char* ssid = "your-ssid";         // Replace with your WiFi SSID
 const char* password = "your-password"; // Replace with your WiFi password
+
+const char* host = "streamer.finance.yahoo.com";
+const uint16_t port = 443;
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,53 @@
 #include "config.h"
 #include <Arduino.h>
+#include <ArduinoHttpClient.h>
 #include <WiFi.h>
 
+WiFiClientSecure wifiClient;
+WebSocketClient wsClient = WebSocketClient(wifiClient, host, port);
+
 void setup() {
-  Serial.begin(115200);
+  Serial1.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
+  Serial1.println("\n");
   while (WiFi.status() != WL_CONNECTED) {
+    Serial1.println("Connecting to WiFi...");
+    WiFi.begin(ssid, password);
     delay(1000);
-    Serial.print(".");
   }
-  Serial.print("\nConnected to WiFi!\nIP Address:");
-  Serial.println(WiFi.localIP());
+  Serial1.println("Connected to WiFi");
+  Serial1.print("IP Address: ");
+  Serial1.println(WiFi.localIP());
 }
 
-void loop() {}
+void loop() {
+  wifiClient.setInsecure();
+  wsClient.begin("/?version=2");
+
+  while (!wsClient.connected()) {
+    Serial1.println("Connecting to websocket...");
+    delay(1000);
+  }
+  Serial1.println("Connected to websocket");
+
+  Serial1.println("Subscribing to BTC-USD ticker...");
+  wsClient.beginMessage(TYPE_TEXT);
+  wsClient.print("{\"subscribe\": [\"BTC-USD\"]}");
+  wsClient.endMessage();
+
+  while (wsClient.connected()) {
+    if (wsClient.parseMessage() > 0) {
+      Serial1.print("Received message: ");
+      Serial1.println(wsClient.readString());
+    } else {
+      Serial1.println("No message received");
+    }
+
+    delay(1000);
+  }
+  Serial1.println("Disconnected");
+  delay(5000);
+}
