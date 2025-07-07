2cedf10e506ae6795f3c5712521852dc619274ef
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-07-07 00:27:36 -0400

    Call Alpaca Markets API and print response

--- include/config.example.h
+++ include/config.example.h
@@ -8,17 +8,29 @@
 
 const char* ssid = "your-ssid";         // Replace with your WiFi SSID
 const char* password = "your-password"; // Replace with your WiFi password
-const char* apcaApiKeyId =
-  "your-alpaca-markets-api-key-id"; // Replace with your Alpaca Markets API key
-                                    // ID
-const char* apcaApiSecretKey =
-  "your-alpaca-markets-api-secret-key"; // Replace with your Alpaca Markets API
-                                        // secret Key
+// Replace with your Alpaca Markets API key ID
+const char* apcaApiKeyId = "your-alpaca-markets-api-key-id";
+// Replace with your Alpaca Markets API secret Key
+const char* apcaApiSecretKey = "your-alpaca-markets-api-secret-key";
 
-// Comma-separated list of symbols to subscribe to
-const size_t maxSymbolsStringLen = 256; // Maximum length of symbols string
-// Maximum number of symbols to subscribe to
+// Maximum length of symbols string
+const size_t maxSymbolsStringLen = 256;
+// Maximum number of symbols to reserve memory for
 const uint16_t maxSymbols = 32;
+// Comma-separated list of symbols to subscribe to
 const char symbols[maxSymbolsStringLen] = "AAPL,GOOGL,MSFT,AMZN,TSLA,NVDA";
+
+// Feed to subscribe to
+// Either sip, iex, delayed_sip, boats, overnight, or otc
+// Only iex or delayed_sip are available with free account
+const char* sourceFeed = "iex";
+
+// Request period in milliseconds, how long to wait before asking for more fresh
+// data for every symbol Free account gets 200 requests per minute, so for max
+// of 32 symbols, we can request data every:
+// (60 seconds / 200 hits) * (32 hits / refresh) = 9.6 seconds
+// MINIMUM refresh time to not hit limit
+// But 30 seconds before requesting again is definitely fast enough for me
+const uint32_t requestPeriod = 30 * 1000;
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,88 @@
 #include "config.h"
 #include <Arduino.h>
+#include <ArduinoHttpClient.h>
+#include <StreamUtils.h>
 #include <WiFi.h>
+
+const size_t maxIDLen = 32;
+// clang-format off
+struct SymbolPrice {
+  char id[maxIDLen];
+  float price;
+  float change;
+  float changePercent;
+};
+// clang-format on
+SymbolPrice allSymbolPrices[maxSymbols];
+uint16_t symbolsCount = 0;
+
+void initializeAllSymbolPrices() {
+  memset(allSymbolPrices, 0, sizeof(allSymbolPrices));
+  char str[maxSymbolsStringLen];
+  strncpy(str, symbols, maxSymbolsStringLen);
+  char* token;
+  char* rest = str;
+  symbolsCount = 0;
+  while ((token = strtok_r(rest, ",", &rest)) && symbolsCount < maxSymbols) {
+    if (strlen(token) < maxIDLen) {
+      strncpy(allSymbolPrices[symbolsCount].id, token, maxIDLen);
+      // If price is negative than no data yet
+      allSymbolPrices[symbolsCount].price = -1;
+      Serial1.printf("Symbol '%s' initialized at index %d\n", token,
+                     symbolsCount);
+      symbolsCount++;
+    } else {
+      Serial1.printf("Symbol '%s' is too long, skipping.\n", token);
+    }
+  }
+}
+
+void updateSymbolPrice(const char* id, float price, float change,
+                       float changePercent) {
+  for (uint16_t i = 0; i < maxSymbols; i++) {
+    if (strcmp(allSymbolPrices[i].id, id) == 0) {
+      allSymbolPrices[i].price = price;
+      allSymbolPrices[i].change = change;
+      allSymbolPrices[i].changePercent = changePercent;
+      Serial1.printf("Updated symbol %s in symbol data list\n",
+                     allSymbolPrices[i].id);
+      return;
+    }
+  }
+  Serial1.printf("Symbol %s not found in symbol data list\n", id);
+}
+
+const size_t maxSymbolDisplayStrLen = 64;
+const size_t maxDisplayStrLen = maxSymbolDisplayStrLen * maxSymbols;
+char displayStr[maxDisplayStrLen];
+
+void updateDisplayStr() {
+  memset(displayStr, 0, maxDisplayStrLen);
+  char* ptr = displayStr;
+  for (uint16_t i = 0; i < symbolsCount; i++) {
+    size_t charsWritten = 0;
+    if (allSymbolPrices[i].price > 0) {
+      char sign = '+';
+      if (allSymbolPrices[i].change < 0) {
+        sign = '-';
+      }
+      charsWritten = snprintf(
+        ptr, maxSymbolDisplayStrLen, "%s: $%.2f %+.2f%% (%c$%.2f)    ",
+        allSymbolPrices[i].id, allSymbolPrices[i].price,
+        allSymbolPrices[i].changePercent, sign, abs(allSymbolPrices[i].change));
+    } else {
+      // No data yet cause price is negative
+      charsWritten = snprintf(ptr, maxSymbolDisplayStrLen,
+                              "%s: No data yet...    ", allSymbolPrices[i].id);
+    }
+    ptr += charsWritten;
+    if (ptr - displayStr >= maxDisplayStrLen - maxSymbolDisplayStrLen) {
+      break; // Prevent overflow
+    }
+  }
+  Serial1.println("Display string updated:");
+  Serial1.println(displayStr);
+}
 
 void setup() {
   Serial1.begin(115200);
@@ -10,6 +92,8 @@ void setup() {
   Serial1.println("\n");
 
   Serial1.println(symbols);
+  initializeAllSymbolPrices();
+  updateDisplayStr();
 }
 
 void loop() {
@@ -22,5 +106,77 @@ void loop() {
   Serial1.print("IP Address: ");
   Serial1.println(WiFi.localIP());
 
+  static uint32_t refreshAtTime;
+  refreshAtTime = 0; // Force refresh on first loop
+  while (WiFi.status() == WL_CONNECTED) {
+    if (refreshAtTime < millis()) {
+      // Time to call API multiple times
+      Serial1.println("Requesting data from Alpaca Markets API...");
+      WiFiClientSecure wifiClient;
+      wifiClient.setInsecure(); // Disable SSL certificate verification for now
+      // The URL is:
+      // https://data.alpaca.markets/v2/stocks/snapshots?symbols={SYMBOLS}&feed={FEED}
+
+      // We need to send something like this:
+      // GET /v2/stocks/snapshots?symbols=AAPL%2CTSLA&feed=iex HTTP/1.1
+      // Accept: application/json
+      // Apca-Api-Key-Id: ********************
+      // Apca-Api-Secret-Key: ****************************************
+      // Host: data.alpaca.markets
+      HttpClient client(wifiClient, "data.alpaca.markets", 443);
+      client.beginRequest();
+      // Length of /v2/stocks/snapshots?symbols=&feed=iex is 38 chars so 64
+      // chars + maxSymbolsStringLen is enough
+      const size_t maxURLLen = 64 + maxSymbolsStringLen;
+      char url[maxURLLen];
+      snprintf(url, maxURLLen, "/v2/stocks/snapshots?symbols=%s&feed=%s",
+               symbols, sourceFeed);
+      Serial1.printf("Requesting https://data.alpaca.markets%s\n", url);
+      client.get(url);
+      client.sendHeader("Accept", "application/json");
+      client.sendHeader("Apca-Api-Key-Id", apcaApiKeyId);
+      client.sendHeader("Apca-Api-Secret-Key", apcaApiSecretKey);
+      client.endRequest();
+      int statusCode = client.responseStatusCode();
+      client.skipResponseHeaders();
+      ReadBufferingClient bufferedClient(client, 256);
+      if (statusCode == 200) { // OK
+        // For now, print response
+        Serial1.println("Response: ");
+        while (bufferedClient.available()) {
+          Serial1.write(bufferedClient.read());
+        }
+      } else {
+        Serial1.printf("Bad status code: %d\n", statusCode);
+        switch (statusCode) {
+          case 400: { // Bad request
+            Serial1.println("Bad request, check your API key and secret.");
+            break;
+          }
+          case 403: { // Forbidden
+            Serial1.println("Forbidden, check your API key and secret.");
+            break;
+          }
+          case 429: { // Too many requests
+            Serial1.println("Too many requests, check your request period.");
+            break;
+          }
+          case 500: { // Internal server error
+            Serial1.println("Internal server error, check Alpaca Markets' "
+                            "Slack or Community Forum.");
+            break;
+          }
+        }
+        Serial1.println("Response: ");
+        while (bufferedClient.available()) {
+          Serial1.write(bufferedClient.read());
+        }
+      }
+      refreshAtTime = millis() + requestPeriod;
+      Serial1.printf("\nNext request in %d seconds\n", requestPeriod / 1000);
+    }
+  }
+
+  Serial1.println("Disconnected from WiFi");
   delay(5000);
 }
