ce22ccce2d111d4d56a1dbce5e5b098da82832c9
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-07-06 22:59:06 -0400

    Nah imagine redoing this all over again

--- include/config.example.h
+++ include/config.example.h
@@ -18,15 +18,7 @@ const char* apcaApiSecretKey =
 // Comma-separated list of symbols to subscribe to
 const size_t maxSymbolsStringLen = 256; // Maximum length of symbols string
 // Maximum number of symbols to subscribe to
-// 30 is the limit for Alpaca Markets free plan on websocket
-const uint16_t maxSymbols = 30;
+const uint16_t maxSymbols = 32;
 const char symbols[maxSymbolsStringLen] = "AAPL,GOOGL,MSFT,AMZN,TSLA,NVDA";
-
-const uint16_t requestPeriod = 30 * 1000; // Request period in milliseconds
-// How often to call the rest API, when not using the websocket
-// Free plan is limited to 200 requests / s
-
-const char* feed = "iex"; // Either "sip", "iex", or "delayed_sip"
-// The SIP feed (all exchanges) is paid
 
 #endif
