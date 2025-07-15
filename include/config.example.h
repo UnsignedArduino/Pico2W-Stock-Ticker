7a7e34d8a1ce050b525f1bc985168470ddef184b
Cyrus Yiu <UnsignedArduino@outlook.com>
2025-07-14 23:41:03 -0400

    Delete unneeded config.example.h options

--- include/config.example.h
+++ include/config.example.h
@@ -8,13 +8,5 @@
 
 // How many **groups of four** 8x8 MAX7219 modules are connected
 const uint8_t matrixModulesCount = 4;
-// How long to wait (in ms) between each shift while scrolling text
-// (lower is faster)
-const uint16_t scrollPeriodSpeed = 30;
-
-// Request period in milliseconds, how long to wait before asking for more data
-// 200 requests / min is rate limit for free account
-// One request is used to download all symbol data
-const uint32_t requestPeriod = 60 * 1000;
 
 #endif
